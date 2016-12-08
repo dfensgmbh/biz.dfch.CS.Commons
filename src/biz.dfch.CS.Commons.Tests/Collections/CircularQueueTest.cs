6d23c314cf0122e24886b2dded29a9911f8bf91e
Ronald Rink <ronald.rink@d-fens.net>
2016-12-08 22:12:06 +0100

    CircularQueue
    * added circular queue

---
+++ src/biz.dfch.CS.Commons.Tests/Collections/CircularQueueTest.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using biz.dfch.CS.Commons.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Commons.Tests.Collections
+{
+    [TestClass]
+    public class CircularQueueTest
+    {
+        public const int CAPACITY = 27;
+
+        [TestMethod]
+        public void EmptyTryDequeueReturnsFalse()
+        {
+            var sut = new CircularQueue<string>();
+            var item = default(string);
+
+            for (var c = 0; c < 1000*1000; c++)
+            {
+                var result = sut.TryDequeue(out item);
+
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public void EmptyTryPeekReturnsFalse()
+        {
+            var sut = new CircularQueue<string>();
+            var item = default(string);
+
+            for (var c = 0; c < 1000*1000; c++)
+            {
+                var result = sut.TryPeek(out item);
+
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public void EnqueueWithTryDequeueCirculates()
+        {
+            var sut = new CircularQueue<string>(CAPACITY);
+
+            for (var c = 0; c < 1000*1000; c++)
+            {
+                var baseItem = "item" + c;
+
+                for (var i = c; i < c + CAPACITY; i++)
+                {
+                    var item = "item" + i;
+                    sut.Enqueue(item);
+
+                    string enqueuedItem;
+
+                    var result = sut.TryPeek(out enqueuedItem);
+                    
+                    Assert.IsTrue(result);
+                    Assert.AreEqual(baseItem, enqueuedItem);
+                }
+
+                c += CAPACITY;
+            }
+        }
+
+        [TestMethod]
+        public void SingleEnqueueSingleDequeueSucceeds()
+        {
+            var stateInfo = new StateInfo
+            {
+                CircularQueue = new CircularQueue<string>(CAPACITY),
+                Count = 1000*1000
+            };
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(DequeueProc), stateInfo);
+
+            Thread.Sleep(1 * 1000);
+
+            EnqueueProc(stateInfo);
+
+            Trace.WriteLine(string.Format("EnqueueOperations '{0}'", stateInfo.EnqueueOperations));
+            Trace.WriteLine(string.Format("DequeueOperations '{0}'", stateInfo.DequeueOperations));
+        }
+
+        public class StateInfo
+        {
+            public CircularQueue<string> CircularQueue { get; set; }
+
+            public int Count;
+            public int EnqueueOperations;
+            public int DequeueOperations;
+        }
+            
+        public static void EnqueueProc(object stateInfo)
+        {
+            Contract.Requires(null != stateInfo);
+            var info = stateInfo as StateInfo;
+            Contract.Assert(null != info);
+
+            for (var c = 0; c < info.Count; c++)
+            {
+                ++info.EnqueueOperations;
+
+                var item = "item" + info.EnqueueOperations;
+
+                info.CircularQueue.Enqueue(item);
+            }
+
+            info.EnqueueOperations = info.Count;
+        }
+
+        public static void DequeueProc(object stateInfo)
+        {
+            Contract.Requires(null != stateInfo);
+            var info = stateInfo as StateInfo;
+            Contract.Assert(null != info);
+
+            var sw = Stopwatch.StartNew();
+
+            do
+            {
+                string item;
+                var result = info.CircularQueue.TryDequeue(out item);
+
+                if (!result)
+                {
+                    continue;
+                }
+
+                info.DequeueOperations++;
+            }
+            while (info.Count >= info.DequeueOperations || info.Count == info.EnqueueOperations);
+        }
+
+        [TestMethod]
+        public void TryDequeueWithWaitTimeSucceeds()
+        {
+            var enqueuedItem = "arbitrary-string";
+            string item;
+            var sut = new CircularQueue<string>(CAPACITY);
+
+            var waitTimeoutMs1 = 1000;
+            var sw1 = Stopwatch.StartNew();
+
+            // try dequeue inexisting item and wait for timeout
+            var result = sut.TryDequeue(out item, waitTimeoutMs1);
+
+            sw1.Stop();
+            Assert.IsFalse(result);
+            Assert.IsTrue(waitTimeoutMs1 <= sw1.ElapsedMilliseconds);
+
+            // dequeue existing item
+            sut.Enqueue(enqueuedItem);
+
+            var waitTimeoutMs2 = 60*1000;
+            var sw2 = Stopwatch.StartNew();
+
+            result = sut.TryDequeue(out item, waitTimeoutMs2);
+
+            sw2.Stop();
+            Assert.IsTrue(result);
+            Assert.IsTrue(waitTimeoutMs2 > sw2.ElapsedMilliseconds);
+            Assert.AreEqual(enqueuedItem, item);
+
+        }
+
+    }
+}
---
+++ src/biz.dfch.CS.Commons/Collections/CircularQueue.cs
@@ -0,0 +1,138 @@
+﻿/**
+ * Copyright 2016 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Threading;
+
+namespace biz.dfch.CS.Commons.Collections
+{
+    public class CircularQueue<T>
+        where T : class
+    {
+        public const int CAPACITY_DEFAULT = 16;
+
+        private volatile bool isInitialised;
+
+        private int enqueuePointer;
+        private int dequeuePointer;
+        private int enqueuedItems;
+        private readonly int capacity;
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+        private readonly List<T> list;
+
+        private readonly object _lock = new object();
+
+        private ManualResetEventSlim manualResetEventSlim = new ManualResetEventSlim(false);
+
+        public CircularQueue()
+            : this(CAPACITY_DEFAULT)
+        {
+            // nothing to be done here
+        }
+
+        public CircularQueue(int capacity)
+        {
+            Contract.Requires(0 < capacity);
+
+            list = new List<T>(capacity);
+            this.capacity = capacity;
+        }
+
+        public void Enqueue(T item)
+        {
+            lock (_lock)
+            {
+                if (!isInitialised)
+                {
+                    list.Add(item);
+
+                    isInitialised = 0 == (enqueuePointer = ++enqueuePointer % capacity);
+                }
+                else
+                {
+                    list[enqueuePointer] = item;
+
+                    enqueuePointer = ++enqueuePointer % capacity;
+                }
+
+                enqueuedItems++;
+                manualResetEventSlim.Set();
+            }
+        }
+
+        public bool TryDequeue(out T item, int waitTimeoutMs)
+        {
+            var result = TryDequeue(out item);
+            if (result)
+            {
+                return true;
+            }
+
+            result = manualResetEventSlim.Wait(waitTimeoutMs);
+            if (!result)
+            {
+                return false;
+            }
+
+            manualResetEventSlim.Reset();
+
+            result = TryDequeue(out item);
+            return result;
+        }
+
+        public bool TryDequeue(out T item)
+        {
+            lock (_lock)
+            {
+                if (0 >= enqueuedItems)
+                {
+                    item = default(T);
+                    return false;
+                }
+
+                item = list[dequeuePointer];
+
+                dequeuePointer = ++dequeuePointer % capacity;
+
+                enqueuedItems--;
+
+                return true;
+            }
+        }
+
+        public bool TryPeek(out T item)
+        {
+            lock (_lock)
+            {
+                if (0 >= enqueuedItems)
+                {
+                    item = default(T);
+                    return false;
+                }
+
+                item = list[dequeuePointer];
+
+                return true;
+            }
+        }
+    }
+}
