c481928f3511b30432fdfa414c4dd9d03207fb12
Ronald Rink <ronald.rink@d-fens.net>
2016-12-13 14:37:26 +0100

    Release 1.8.1
    * updated version
    * updated release notes

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -14,4 +14,4 @@ using System.Reflection;
 // NOTE
 // also update the corresponding nuspec file when changing the version
 //
-[assembly: AssemblyVersion("1.8.0.*")]
+[assembly: AssemblyVersion("1.8.1.*")]
